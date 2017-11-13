f5711bc99f03e7dd1f7fa72d5646b38902042110
Lucas Calazans <lucas.calazans@webjump.com.br>
2017-11-13 16:23:05 -0200

    Fixing ctrl+c ctrl+v files

--- Plugin/Model/TemplateEngine/DebugHints.php
+++ Plugin/Model/TemplateEngine/DebugHints.php
@@ -1,24 +1,52 @@
 <?php
 
-namespace Webjump\PathHint\Plugin\Model\TemplateEngine;
+namespace LucasCalazans\PathHint\Plugin\Model\TemplateEngine;
 
+use Magento\Store\Model\ScopeInterface;
 use Magento\Framework\App\ObjectManager;
+use Magento\Framework\App\RequestInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Developer\Helper\Data as DevHelper;
 use Magento\Framework\View\TemplateEngineFactory;
 use Magento\Framework\View\TemplateEngineInterface;
-use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Developer\Model\TemplateEngine\Plugin\DebugHints as DeveloperDebugHints;
+use Magento\Developer\Model\TemplateEngine\Decorator\DebugHintsFactory;
 
-class DebugHints extends \Magento\Developer\Model\TemplateEngine\Plugin\DebugHints {
-    
+class DebugHints extends DeveloperDebugHints {
+
+    protected $_requestInterface;
+
+    /**
+     * @param RequestInterface $requestInterface
+     * @param ScopeConfigInterface $scopeConfig
+     * @param StoreManagerInterface $storeManager
+     * @param DevHelper $devHelper
+     * @param DebugHintsFactory $debugHintsFactory
+     * @param string $debugHintsPath
+     */
+    public function __construct(
+        RequestInterface $requestInterface,
+        ScopeConfigInterface $scopeConfig,
+        StoreManagerInterface $storeManager,
+        DevHelper $devHelper,
+        DebugHintsFactory $debugHintsFactory,
+        $debugHintsPath
+    ) {
+        $this->_requestInterface = $requestInterface;
+        parent::__construct($scopeConfig, $storeManager, $devHelper, $debugHintsFactory, $debugHintsPath);
+    }
+
     public function afterCreate(
         TemplateEngineFactory $subject,
         TemplateEngineInterface $invocationResult
     ) {
-        $params = ObjectManager::getInstance()->get('Magento\Framework\App\RequestInterface')->getParams();
-        
+        $params = $this->_requestInterface->getParams();
+
         $storeCode = $this->storeManager->getStore()->getCode();
-        if (isset($params['enablepath']) || 
+        if (isset($params['enablepath']) ||
             ($this->scopeConfig->getValue(
-            $this->debugHintsPath, ScopeInterface::SCOPE_STORE, $storeCode) && 
+            $this->debugHintsPath, ScopeInterface::SCOPE_STORE, $storeCode) &&
             $this->devHelper->isDevAllowed())) {
 
             $showBlockHints = ((isset($params['enablepath']) && $params['enablepath'] == 1) || $this->scopeConfig->getValue(
--- registration.php
+++ registration.php
@@ -1,13 +1,11 @@
 <?php
 /**
- * @author      Webjump Core Team <dev@webjump.com.br>
- * @copyright   2017 Webjump (http://www.webjump.com.br)
- * @license     http://www.webjump.com.br  Copyright
+ * @author      Lucas Calazans <contato@lucascalazans.com.br>
+ * @license     MIT
  *
- * @link        http://www.webjump.com.br
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Webjump_PathHint',
+    'LucasCalazans_PathHint',
     __DIR__
 );
